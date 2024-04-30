4a0c1d4702c9d20b3d2d858113a6f44111db2447
Christopher Lamar Powe <33496540+clpowe@users.noreply.github.com>
2024-04-30 03:35:03 +0000

    late night

--- examples/dom/index.js
+++ examples/dom/index.js
@@ -1,2 +1,20 @@
 const find_me = document.querySelector('#find_me')
 console.log("Find Me", find_me)
+
+const find_by_id = document.getElementById("find_me")
+console.log("find_by_id", find_by_id)
+
+const change_me = document.getElementById('change_me')
+
+change_me.innerText = "Changed!"
+
+const add_html = document.getElementById('add_html')
+
+add_html.innerHTML = "<h3>This is added via Javascript</h3>"
+
+const find_all = document.querySelectorAll('.change-all')
+console.log("find_all", find_all)
+
+find_all.forEach((item, index) => {
+    item.innerText = "Added from forEach - " + index
+})
---
+++ examples/events/index.js
@@ -0,0 +1,29 @@
+const click_me = document.getElementById("click_me")
+const click_value = document.getElementById("click_value")
+const change_me = document.getElementById("change_me")
+
+
+let value = parseInt(click_value.value)
+
+
+click_me.addEventListener("click", () =>  {
+    value += 1
+    click_value.value = value
+})
+
+change_me.addEventListener("change", () => {
+    console.log("change", change_me.value)
+})
+
+change_me.addEventListener("blur", () => {
+    console.log("blur", change_me.value)
+})
+
+change_me.addEventListener("input", (event) => {
+    console.log("input", event)
+})
+
+change_me.addEventListener("beforeinput", () => {
+    console.log("beforeinput", change_me.value)
+})
+
